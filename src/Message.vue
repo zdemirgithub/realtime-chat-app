d1742a6f039a9717101945399f4210b42843b953
Zehra Demir <zehrademir.gm@gmail.com>
2024-12-15 18:13:19 +0300

    Create Message.vue

---
+++ src/Message.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="message">
+    <strong>{{ message.user }}:</strong>
+    <span>{{ message.text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  props: {
+    message: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.message {
+  display: flex;
+  justify-content: flex-start;
+  padding: 10px;
+  margin-bottom: 10px;
+  background-color: #f1f1f1;
+  border-radius: 5px;
+}
+strong {
+  margin-right: 10px;
+  color: #333;
+}
+span {
+  color: #555;
+}
+</style>
